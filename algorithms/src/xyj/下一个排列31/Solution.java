efc34a45b433e3efa13c3c9558a12b280153ef29
“xuyaojian123” <2450703254@qq.com>
2022-06-06 20:27:36 +0800

    2022-6-6 下一个排列

---
+++ algorithms/src/xyj/下一个排列31/Solution.java
@@ -0,0 +1,71 @@
+package xyj.下一个排列31;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    //没有重复元素（按照字典序列生成全排列）
+    public List<List<Integer>> permutation(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        backtracking(result,nums,0);
+        return result;
+
+    }
+    public void backtracking(List<List<Integer>> result, int[] nums,int t){
+        if (t == nums.length){
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i]+" ");
+            }
+            System.out.println();
+        }else {
+            for (int i = t;i<nums.length;i++){
+                Arrays.sort(nums,t,nums.length);
+                swap(nums,t,i);
+                backtracking(result,nums,t+1);
+                swap(nums,t,i);
+            }
+        }
+    }
+    public void swap(int[] nums,int a,int b){
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
+    }
+
+    // 按照字典序列,找到下一个排列
+    public void nextPermutation(int[] nums) {
+        int ans = 0,sym=0;
+        int flag = 0;
+        for (int i = nums.length-1; i >=1; i--) {
+            if (nums[i] > nums[i-1]){
+                ans = i-1;
+                sym=1;
+                break;
+            }
+        }
+        for (int i = nums.length-1; i >=0; i--) {
+            if (nums[i] > nums[ans]){
+                flag = i;
+                break;
+            }
+        }
+        swap(nums,ans,flag);
+        if (sym == 0){
+            ans = -1;
+        }
+        Arrays.sort(nums, ans+1,nums.length);
+    }
+
+    public static void main(String[] args) {
+        //int[] nums = {1,2,3,4,5};
+        int[] nums = {3,2,1}    ;
+        Solution solution = new Solution();
+        solution.nextPermutation(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]+" ");
+        }
+        System.out.println();
+    }
+}
